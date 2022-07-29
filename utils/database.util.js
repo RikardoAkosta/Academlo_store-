95e3941e95eefd429579207890474d6be595b730
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-07-29 15:53:45 +0200

    se actualizan variables de entorno

--- utils/database.util.js
+++ utils/database.util.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './confidential.env' });
+dotenv.config({ path: './config.env' });
 
 const db = new Sequelize({
   dialect: 'postgres',
